8ae63e4a4cb258c3acc04d73d2271ad262a94517
Venray-std <Venray.Kong@outlook.com>
2021-01-07 22:02:11 +0800

    linearRegress

--- Python/ Quantile.py
+++ Python/ Quantile.py
@@ -20,9 +20,9 @@ from scipy.stats import f
 x_norm = norm.ppf(0.95)  # 标准正态分布，下侧分位数
 
 x_chi = chi2.ppf(0.9,df=4)  # 卡方分布，默认是下侧分位数
-x_f = f.ppf(0.9,dfn=1,dfd=4) # f分布 下侧分位数
+x_f = f.ppf(0.95,dfn=2,dfd=18) # f分布 下侧分位数
 x_t = t.ppf(0.95,df=4)  # t 分布默认是下侧分位数
 print ("norm distribute", x_norm)
 print ("chi-square distribute", x_chi)
-print ("f distribute", x_f)
 print ("t distribute", x_t)
+print ("f distribute", x_f)
